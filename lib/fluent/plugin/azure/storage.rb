8eb71edc52df27bffac20a051004a6c10ffedbe8
sys_cat <systemcat91@gmail.com>
2016-03-09 20:09:26 +0900

    add files

---
+++ lib/fluent/plugin/azure/storage.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/azure/storage/version"
+
+module Fluent
+  module Plugin
+    module Azure
+      module Storage
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/fluent/plugin/azure/storage_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Fluent::Plugin::Azure::Storage do
+  it 'has a version number' do
+    expect(Fluent::Plugin::Azure::Storage::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
