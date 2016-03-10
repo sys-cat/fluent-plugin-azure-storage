4e8f708c24e81cf962903cb9604f1a4762a7ce0b
sys_cat <systemcat91@gmail.com>
2016-03-10 17:43:27 +0900

    modify base class.

--- lib/fluent/plugin/out_azure-storage.rb
+++ lib/fluent/plugin/out_azure-storage.rb
@@ -1,4 +1,31 @@
-module Fluent
-    class AzureStorageOutput < Fluent::BufferedOutput
+class Fluent::AzureStorageOutput < Fluent::BufferedOutput
+    Fluent::Plugin.register_output('azurestorage', self)
+
+    def initialize
+        super
+    end
+
+    def configure(conf)
+        super
+    end
+
+    def start
+        super
+    end
+
+    def shutdown
+        super
+    end
+
+    def format(tag, time, record)
+        [tag, time, record].to_msgpack
+    end
+
+    def write(chunk)
+        records = []
+        chunk.msgpach_each do |record|
+            #records << record
+        end
+        write records
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'fluent/plugin/azure/storage'
+require 'fluent/plugin/'
